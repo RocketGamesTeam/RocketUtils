bbee508d7d05987dddb3c00c17a8e89ad5e13266
AhmetUgur <ahmet@panteon.com.tr>
2020-07-02 18:01:43 +0000

    signletonbehaviour added

---
+++ Runtime/SingletonBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace RocketUtils
+{
+    public class SingletonBehaviour<T> : MonoBehaviour where T : MonoBehaviour
+    {
+        public static T Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = (T) FindObjectOfType(typeof(T));
+                    if (_instance == null)
+                    {
+                        Debug.LogError("An instance of " + typeof(T) +
+                                                   " is needed in the scene, but there is none.");
+                    }
+                }
+
+                return _instance;
+            }
+        }
+
+        private static T _instance;
+    }
+}
