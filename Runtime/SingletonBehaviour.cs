5c03171dd79b9eaf36c9d6f47c273d2436e93fbb
Ahmet Ugur <ahmetugur@panteon.games>
2020-09-08 15:00:36 +0000

    debug label added in debug mode

--- Runtime/SingletonBehaviour.cs
+++ Runtime/SingletonBehaviour.cs
@@ -4,6 +4,8 @@ namespace RocketUtils
 {
     public abstract class SingletonBehaviour<T> : MonoBehaviour where T : MonoBehaviour
     {
+        protected bool DrawDebugLabel;
+
         private void Awake()
         {
             if (Instance == null)
@@ -28,5 +30,60 @@ namespace RocketUtils
         protected abstract void OnAwake();
 
         public static T Instance { get; private set; }
+
+#if ROC_DEBUG_MODE
+        void OnGUI()
+        {
+            if(!DrawDebugLabel) return;
+
+            DrawTextWithOutline(new Rect(15, Screen.height - 35, 300, 80), "Rocket Debug", new GUIStyle(), Color.white, Color.black, 1);
+        }
+
+        void DrawTextWithOutline(Rect centerRect, string text, GUIStyle style, Color borderColor, Color innerColor, int borderWidth)
+        {
+            // assign the border color
+            style.normal.textColor = borderColor;
+            style.fontSize = 24;
+
+            // draw an outline color copy to the left and up from original
+            Rect modRect = centerRect;
+            modRect.x -= borderWidth;
+            modRect.y -= borderWidth;
+            GUI.Label(modRect, text, style);
+
+
+            // stamp copies from the top left corner to the top right corner
+            while (modRect.x <= centerRect.x + borderWidth)
+            {
+                modRect.x++;
+                GUI.Label(modRect, text, style);
+            }
+
+            // stamp copies from the top right corner to the bottom right corner
+            while (modRect.y <= centerRect.y + borderWidth)
+            {
+                modRect.y++;
+                GUI.Label(modRect, text, style);
+            }
+
+            // stamp copies from the bottom right corner to the bottom left corner
+            while (modRect.x >= centerRect.x - borderWidth)
+            {
+                modRect.x--;
+                GUI.Label(modRect, text, style);
+            }
+
+            // stamp copies from the bottom left corner to the top left corner
+            while (modRect.y >= centerRect.y - borderWidth)
+            {
+                modRect.y--;
+                GUI.Label(modRect, text, style);
+            }
+
+            // draw the inner color version in the center
+            style.normal.textColor = innerColor;
+            GUI.Label(centerRect, text, style);
+        }
+#endif
     }
 }
