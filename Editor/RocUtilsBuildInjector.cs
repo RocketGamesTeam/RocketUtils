97bb06a23e6b3728eb03a8b22e76e7db8fa7a8aa
Ahmet Ugur <ahmetugur@panteon.games>
2021-02-11 14:53:44 +0000

    build injector added.

---
+++ Editor/RocUtilsBuildInjector.cs
@@ -0,0 +1,36 @@
+﻿#if UNITY_IOS
+using UnityEditor.iOS.Xcode;
+#endif
+
+using System.IO;
+using UnityEditor;
+using UnityEditor.Callbacks;
+using UnityEngine;
+
+namespace RocketUtils.Editor
+{
+	public class RocAdBuildInjector
+	{
+		[PostProcessBuild(1000)]
+		public static void OnPostprocessBuild(BuildTarget buildTarget, string path)
+		{
+#if UNITY_IOS
+            if (buildTarget != BuildTarget.iOS) return;
+
+            // Get plist
+            string plistPath = string.Format("{0}{1}Info.plist", path, Path.DirectorySeparatorChar);
+			PlistDocument plist = new PlistDocument();
+			plist.ReadFromString(File.ReadAllText(plistPath));
+
+			// Get root
+			PlistElementDict rootDict = plist.root;
+
+		    rootDict.SetString("NSUserTrackingUsageDescription", "This identifier will be used to deliver personalized ads to you.");
+
+            // Write to file
+            File.WriteAllText(plistPath, plist.WriteToString());
+#endif
+        }
+    }
+}
+
