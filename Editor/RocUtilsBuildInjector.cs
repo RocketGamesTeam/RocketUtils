81562581aadeed3446f85f261f7eced4ee3bfd40
Ahmet Ugur <ahmetugur@panteon.games>
2021-03-16 15:54:27 +0000

    AppTrackingTransparency added as framework

--- Editor/RocUtilsBuildInjector.cs
+++ Editor/RocUtilsBuildInjector.cs
@@ -11,7 +11,9 @@ namespace RocketUtils.Editor
 {
 	public class RocAdBuildInjector
 	{
-		[PostProcessBuild(1000)]
+	    private const string _defaultTargetName = "Unity-iPhone";
+
+        [PostProcessBuild(1000)]
 		public static void OnPostprocessBuild(BuildTarget buildTarget, string path)
 		{
 #if UNITY_IOS
@@ -29,6 +31,21 @@ namespace RocketUtils.Editor
 
             // Write to file
             File.WriteAllText(plistPath, plist.WriteToString());
+
+		    var projPath = PBXProject.GetPBXProjectPath(path);
+		    var xcodeProject = new PBXProject();
+		    xcodeProject.ReadFromFile(projPath);
+
+		    Debug.Log(string.Format("PBX Project path: {0}", (object)projPath));
+#if UNITY_2019_3_OR_NEWER
+			var targetGuid = xcodeProject.GetUnityMainTargetGuid();
+#else
+		    var targetGuid = xcodeProject.TargetGuidByName(_defaultTargetName);
+#endif
+
+		    Debug.Log(string.Format("Target GUID: {0}", targetGuid));
+		    xcodeProject.AddFrameworkToProject(targetGuid, "AppTrackingTransparency.framework", true);
+		    xcodeProject.WriteToFile(projPath);
 #endif
         }
     }
