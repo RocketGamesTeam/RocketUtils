e34ae7bce226bee5b6a701fc4b04ef86c1dd3058
CagkanCaglayanel <cagkancaglayanel@panteon.games>
2022-04-13 09:34:36 +0300

    AdFormatExtensions added
    ToAdFormat converts adFormat strings to AdFormat

---
+++ Runtime/Extensions/AdFormatExtensions.cs
@@ -0,0 +1,21 @@
+﻿using RocketUtils.Models;
+
+namespace RocketUtils.Extensions
+{
+    public static class AdFormatExtensions
+    {
+        public static AdFormat ToAdFormat(this string adFormat)
+        {
+            if (adFormat.Contains("banner"))
+                return AdFormat.Banner;
+
+            if (adFormat.Contains("interstitial"))
+                return AdFormat.Interstitial;
+
+            if (adFormat.Contains("rewarded"))
+                return AdFormat.RewardedVideo;
+            
+            return AdFormat.None;
+        }
+    }
+}
